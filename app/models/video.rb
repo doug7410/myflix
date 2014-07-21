78313d095851ec962fdcb72460f1baee3f23a658
Doug Steinberg <dstein-phins@hotmail.com>
2014-07-21 16:46:32 +0000

    search by title in video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,10 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+    Video.where('title LIKE ?', "%#{search_term}%")
+  end
 end
 
 
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,21 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
-end
+  it "finds no matches from search" do
+    vid1 = Video.create(title: 'Family Guy', description: "a funny show")
+    vid2 = Video.create(title: 'Futurame', description: "A funny show about the future.")
+    expect(Video.search_by_title('monk')).to eq([])
+  end
+
+  it "finds exactly one match from search" do
+    vid1 = Video.create(title: 'Family Guy', description: "a funny show")
+    vid2 = Video.create(title: 'Futurama', description: "A funny show about the future.")
+    expect(Video.search_by_title('Family').first).to eq(vid1)
+  end
+
+  it "finds multiple matches from search" do
+    vid1 = Video.create(title: 'Family Guy', description: "a funny show")
+    vid2 = Video.create(title: 'Futurama', description: "A funny show about the future.")
+    expect(Video.search_by_title('f')).to eq([vid1,vid2])
+  end
+end 
