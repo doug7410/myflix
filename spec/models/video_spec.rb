11c64e0c3f289e6e48ec299a555319f7bf67a264
Doug Steinberg <dstein-phins@hotmail.com>
2014-07-22 01:18:12 +0000

    search by title in video model after solutions

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,21 +6,30 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
-  it "finds no matches from search" do
-    vid1 = Video.create(title: 'Family Guy', description: "a funny show")
-    vid2 = Video.create(title: 'Futurame', description: "A funny show about the future.")
-    expect(Video.search_by_title('monk')).to eq([])
-  end
+  describe "search by title" do
+    
+    let!(:vid1) { Video.create(title: 'Family Guy', description: 'a funny show', created_at: 1.day.ago) }
+    let!(:vid2) { Video.create(title: 'Futurama', description: "A funny show about the future." )}
 
-  it "finds exactly one match from search" do
-    vid1 = Video.create(title: 'Family Guy', description: "a funny show")
-    vid2 = Video.create(title: 'Futurama', description: "A funny show about the future.")
-    expect(Video.search_by_title('Family').first).to eq(vid1)
-  end
+    it "returns an empty array in there is no match" do
+      expect(Video.search_by_title('monk')).to eq([])
+    end
 
-  it "finds multiple matches from search" do
-    vid1 = Video.create(title: 'Family Guy', description: "a funny show")
-    vid2 = Video.create(title: 'Futurama', description: "A funny show about the future.")
-    expect(Video.search_by_title('f')).to eq([vid1,vid2])
+    it "returns an array of one video for an exact match" do
+      expect(Video.search_by_title('Family').first).to eq(vid1)
+    end
+
+    it "returns and array of one video for a partial match" do 
+      expect(Video.search_by_title('mily').first).to eq(vid1)
+    end
+
+    it "returns am array of all matches ordered by created at" do
+      expect(Video.search_by_title('f')).to eq([vid2,vid1])
+    end
+
+    it "returns an empty array for a search with an empty string" do
+      expect(Video.search_by_title('')).to eq([])
+    end
+
   end
 end 
