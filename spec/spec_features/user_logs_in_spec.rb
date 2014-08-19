faf42f4d182e76a7b1b336401ee72e745090fc86
Doug Steinberg <dstein-phins@hotmail.com>
2014-08-19 15:29:54 +0000

    add capybara and feature spec for user log in

---
+++ spec/spec_features/user_logs_in_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'User logs in' do
+  scenario "with valid email and password" do
+    bob = Fabricate(:user)
+    visit sessions_new_path
+    fill_in "Email", with: bob.email
+    fill_in "Password", with: bob.password
+    click_button "Sign In"
+    expect(page).to have_content bob.full_name
+  end
+end
