18e713fe3a6fadd24729f7af8a79d98861fb248d
Doug Steinberg <dstein-phins@hotmail.com>
2014-08-21 20:45:30 +0000

    remove unnecciary code

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,6 @@ class Video < ActiveRecord::Base
   has_many :reviews , -> { order "created_at DESC" }
   has_many :queue_items
 
-has_many :sssss
   validates_presence_of :title, :description
 
   def self.search_by_title(search_term)
@@ -13,7 +12,6 @@ has_many :sssss
       Video.where('title LIKE ?', "%#{search_term}%").order("created_at ASC")
     end
   end
- :aaaaa
   def average_rating
     reviews.average(:rating).round(2) if reviews.average(:rating)
   end 
