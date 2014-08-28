ed1a22e68091423762db642763f81e9164e7e490
Doug Steinberg <dstein-phins@hotmail.com>
2014-08-28 14:27:43 +0000

    social media feature spec

---
+++ spec/spec_features/interact_with_social_medaia_features_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+feature "user interacts with social media features"  do
+  
+  given(:bob) { Fabricate(:user) }
+  given(:tom) { Fabricate(:user) }
+  given(:comedies) { Fabricate(:category) }
+  given(:the_office) { Fabricate(:video, category: comedies) }
+  given!(:review) { Fabricate(:review, user: tom, video: the_office) }
+
+  scenario "follow and unfollow a user" do
+    log_in_user(bob)
+    click_on_a_video(the_office)
+    click_on_the_first_review_user
+    expect_to_be_on_the_users_page(tom)
+    follow_a_user(tom)
+    expect_the_user_to_be_on_people_page(tom)
+    un_follow_a_user(tom)
+    expect_a_user_not_to_be_on_people_page(tom)
+  end
+end
+
+def  expect_a_user_not_to_be_on_people_page(user)
+  expect(page).not_to have_link(user.full_name)
+end
+
+def un_follow_a_user(user)
+  find("a[href='/relationships/#{user.id}']").click
+end
+
+def  expect_the_user_to_be_on_people_page(user)
+  visit people_path
+  expect(page).to have_link(user.full_name)
+end
+
+def follow_a_user(user)
+  find("a[href='/follow_person/#{user.id}']").click
+end
+
+def expect_to_be_on_the_users_page(user)  
+  expect(current_path).to eq(user_path(user)) 
+end
+
+def click_on_a_video(video)
+  visit home_path
+  find("a[href='/videos/#{video.id}']").click
+end
+
+def click_on_the_first_review_user
+  find("a[href='/users/#{tom.id}']").click
+end
