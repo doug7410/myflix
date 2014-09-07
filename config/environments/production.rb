1e57cb688d67cb0d81b8099dfa709d6fd04287a0
Doug Steinberg <dstein-phins@hotmail.com>
2014-09-07 00:25:20 -0400

    added default url for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { host: 'pacific-badlands-4137.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
