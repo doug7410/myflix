d48a67a7ef13a637f2e1918190c5a701dcfdef0c
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-09 22:43:00 -0500

    updated initializer with mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,14 @@ Myflix::Application.configure do
   config.assets.digest = true
 
   config.i18n.fallbacks = true
-
+ 
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { host: 'pacific-badlands-4137.herokuapp.com'}
+  
+  if Rails.env.production?
+    config.action_mailer.default_url_options = { host: 'myflix-doug.herokuapp.com'}
+  else
+    config.action_mailer.default_url_options = { host: 'doug-myflix-staging.herokuapp.com'}
+  end
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
